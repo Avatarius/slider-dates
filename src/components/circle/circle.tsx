1ef6aeaf6e1744b4be5a862031789394c048441d
Chingis Choybsonov <avatarius93@gmail.com>
2024-10-09 03:41:18 +0300

    статично расположены элементы на круге

--- src/components/circle/circle.tsx
+++ src/components/circle/circle.tsx
@@ -1,20 +1,44 @@
-import { IHistoricalData } from '../../utils/types';
-import styles from './circle.module.scss';
-
+import { useEffect, useRef, useState } from "react";
+import { IHistoricalData } from "../../utils/types";
+import styles from "./circle.module.scss";
 
 interface ICircleProps {
   data: IHistoricalData[];
 }
 
-function Circle({data} : ICircleProps) {
-  const dataArray: IHistoricalData[] = data.length >= 6 ? data.slice(0, 6) : data;
-  const buttonsArray = dataArray.map((item, index) => <button key={item.id} className={styles.button}>{index + 1}</button>);
+function Circle({ data }: ICircleProps) {
+  const [circleWidth, setCircleWidth] = useState(0);
+  useEffect(() => {
+    if (circleRef.current?.offsetWidth) {
+      setCircleWidth(circleRef.current.getBoundingClientRect().width);
+
+    }
+  }, []);
+
+  function calculatePos(index: number) {
+    const step = (2 * Math.PI) / data.length;
+    const angle = step * index;
+    const x = circleWidth / 2  * Math.cos(angle);
+    const y = circleWidth / 2 * Math.sin(angle);
+    return {x, y};
+  }
+
+
+  const circleRef = useRef<HTMLDivElement>(null);
+  const dataArray: IHistoricalData[] =
+  data.length >= 6 ? data.slice(0, 6) : data;
+  const buttonsArray = dataArray.map((item, index) => {
+    const {x, y} = calculatePos(index);
+    return (<button key={item.id} className={styles.button} style={{translate: `${x}px ${y}px`}}>
+      {index + 1}
+    </button>)
+  });
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={circleRef}>
       {buttonsArray}
     </div>
-  )
+  );
 }
 
-export {Circle};
+export { Circle };
--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -2,18 +2,18 @@ import { ArrowButton } from "../arrowButton/arrowButton";
 import { Circle } from "../circle/circle";
 import styles from "./slider.module.scss";
 import { historicalData } from "../../utils/constants";
-import clsx from "clsx";
+import { useEffect, useRef } from "react";
 
 function Slider() {
   return (
     <section className={styles.container}>
-      {/* <Circle data={historicalData}/> */}
+      <Circle data={historicalData} />
       <h1 className={styles.title}>Исторические даты</h1>
       <div className={styles.controls}>
         <p className={styles.controls__slide}>06/06</p>
-        <div className={styles['controls__button-container']}>
-          <ArrowButton side={false}/>
-          <ArrowButton side={true}/>
+        <div className={styles["controls__button-container"]}>
+          <ArrowButton side={false} />
+          <ArrowButton side={true} />
         </div>
       </div>
     </section>
